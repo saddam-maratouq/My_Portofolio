1ef8fea235f0ed30c8af657a11216c1b0c7d4cf6
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-01-14 22:54:55 +0200

    finsh 2 sec from home page

---
+++ src/page/Home/FirstSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react' 
+import '../Home/home.css'  
+import {FaGithub}  from 'react-icons/fa'  
+import {FaLinkedin}  from 'react-icons/fa'  
+
+
+
+
+
+
+const FirstSection = () => {
+    return (
+        <section className='home' id='Main-page'>  
+        <nav>
+        <h2> Saddam <span> maratouq </span>  </h2>  
+        <ul> 
+            <li> <a href=""> Home </a> </li>
+            <li> <a href=""> Project  </a> </li>
+            <li> <a href="">  Resume </a> </li>
+        </ul>
+        </nav> 
+
+        <div className="container"> 
+         <div className="content">
+             <h3>  Hello there,  🤝 I'm  </h3> <br /> <br />
+             <h1> Saddam <span> maratouq</span> </h1> <br /> <br /> 
+             <h2> I'm <span> Frontend  </span> develober   </h2>  
+             <a href="" className="btn"> Download CV </a> 
+             <br /> <br /> 
+            <div className='icon' >      
+       
+             <a href=" https://github.com/saddam-maratouq?tab=repositories" target='_blank' >   <FaGithub size='3em'/>  </a>  
+             <a href="https://www.linkedin.com/in/saddam-maratouq-51838a206/"  target='_blank'>      <FaLinkedin size='3em'/>  </a>  
+
+            </div>  
+
+         <div className="hero">  
+             <img src="images/main.png" alt="hero image" />
+           </div>
+            
+         </div> 
+        </div>
+    </section>
+    )
+}
+
+export default FirstSection
---
+++ src/page/Home/SecSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './SecSection.css' 
+
+
+const SecSection = () => {
+    return (
+       <section className='About' >
+           
+        <div className='codeing-Img'>  <img src="images/Programming.png" alt="" />  </div> 
+
+        <div className='About-info' >
+        <h2> About  <span> me  </span>   </h2> 
+        <h1> Frontend developer   </h1>
+        <p>  
+        I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️ 
+        
+        </p>
+        </div>
+       </section>
+    )
+}
+
+export default SecSection
