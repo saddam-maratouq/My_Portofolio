2cfb1d56c119ff29c37ece2d9cee5beedb8b8e33
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-01-16 18:53:01 +0200

    finsh project page still need to map Im totaly forgat also make fotter and add email hire butoon or mapy section next time and resume page

--- src/page/Project.jsx
+++ src/page/Project.jsx
@@ -1,10 +1,72 @@
 import React from 'react'
+import './project/Project.css' 
+
+//Images     
+import ExpenseTracker from '../../images/Ex.jpg' 
+import CatStore from '../../images/pro1C.jpg' 
+import DiscoverCity from '../../images/pro2Ci.jpg' 
+
+//icons  
+import {FaGithubAlt}  from 'react-icons/fa'  
+
+import { SiNetlify } from "react-icons/si";
+
+
+
+  
 
 const Project = () => {
     return (
-        <div>
-            <h1> project </h1>
+<section className='projects'> 
+    <h1 className='title'> projects </h1> 
+    <div className="contentainer">
+
+    <div className="projects-cards">
+        <div className="projects-img">
+        <img src={CatStore}  alt=" catStore " />
+        </div> 
+
+        <div className="project-info">
+                <h2>  Cat store  </h2> 
+                <a href=" https://cat-store-96.netlify.app/Producet " target='_blank' >  <SiNetlify size='2em' /> </a> 
+                <a href=" https://github.com/saddam-maratouq/Cat_Store " target='_blank' >  <FaGithubAlt size='2em'  />   </a> 
+
         </div>
+    </div>  
+
+    <div className="projects-cards">
+        <div className="projects-img">
+        <img src={ExpenseTracker}  alt=" money count  " />
+        </div> 
+
+        <div className="project-info">
+                <h2>  Expense Tracker </h2> 
+                <a href=" https://expense-traker-96.netlify.app/ " target='_blank' >  <SiNetlify size='2em' /> </a> 
+                <a href=" https://github.com/saddam-maratouq/Expense_Tracker " target='_blank' >  <FaGithubAlt size='2em'  />   </a> 
+
+        </div>
+    </div> 
+
+    <div className="projects-cards">
+        <div className="projects-img">
+        <img src={DiscoverCity}  alt="city map " />
+        </div> 
+
+        <div className="project-info">
+                <h2> Discover City </h2> 
+                <a href="https://discover-city-sq.netlify.app/
+                   " target='_blank' >  <SiNetlify size='2em' /> </a> 
+                <a href="https://github.com/saddam-maratouq/Discover-city" target='_blank' >  <FaGithubAlt size='2em'  />   </a> 
+
+        </div>
+    </div> 
+
+    
+ 
+    </div> 
+
+</section>  
+
     )
 }
 
