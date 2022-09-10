8ba282d3bb0b796d0916a33e669459c61b0b648b
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-09-10 00:29:10 -0400

    resize image size for performance

--- Data/CardData.js
+++ Data/CardData.js
@@ -4,48 +4,48 @@ export const CardData = [
     {
     id: 1,
     title: 'Instgram',
-    Image: 'https://i.ibb.co/Rpwp8hg/crop-Insta.jpg',
+    Image: 'https://i.ibb.co/0DFQChq/crop-insta-optimized.jpg',
     github: 'https://github.com/saddam-maratouq/R-N-Instagram',
    
+   },
+   {
+       id: 3,
+       title: 'Cat Store',
+       Image: 'https://i.ibb.co/JtwJkQ4/cat-1-optimized.jpg', 
+       github: 'https://github.com/saddam-maratouq/Cat_Store/tree/Normal',
+       netlify: 'https://cat-lover.netlify.app/home',
    },
     {
         id: 2,
         title: 'Sq food',
-        Image: 'https://i.ibb.co/wMLRFKt/cropFood.jpg', 
+        Image: 'https://i.ibb.co/NrcHx9r/cropfood-optimized.jpg', 
         github: 'https://github.com/saddam-maratouq/R.N-FoodApp',
     },
     
-    {
-        id: 3,
-        title: 'Cat Store',
-        Image: 'https://i.ibb.co/5vwJpVM/cass-1.jpg', 
-        github: 'https://github.com/saddam-maratouq/Cat_Store/tree/Normal',
-        netlify: 'https://cat-lover.netlify.app/home',
-    },
 
+    {
+        id: 6,
+        title: 'Point of Sale',
+        Image: 'https://i.ibb.co/gFcgHKD/Main-food.jpg', 
+        github: 'https://github.com/saddam-maratouq/point-of-sale_RJS', 
+        netlify: 'https://point-of-sale96.netlify.app',
+    },
     {
         id: 4,
         title: 'Expense Tracker',
-        Image: 'https://i.ibb.co/ncDdpdg/Expense.jpg', 
+        Image: 'https://i.ibb.co/wWCgbD4/expense-optimized.jpg', 
         github: 'https://github.com/saddam-maratouq/Expense_Tracker',
         netlify: 'https://expense-traker-96.netlify.app/',
     },
 
     {
         id: 5,
-        title: 'Discover city',
-        Image: 'https://i.ibb.co/zfXKb4d/pro2Ci.jpg', 
+        title: 'Discover City',
+        Image: 'https://i.ibb.co/6ng65Rn/exploler-map-optimized.jpg', 
         github: 'https://github.com/saddam-maratouq/Discover-city', 
         netlify: 'https://discover-city-sq.netlify.app/',
     },
 
-    {
-        id: 6,
-        title: 'Electro store',
-        Image: 'https://i.ibb.co/0tSP9v9/pro3El.jpg', 
-        github: 'https://github.com/saddam-maratouq/elctro', 
-        netlify: 'https://saddam-maratouq.github.io/elctro/',
-    },
 
 ]
 
